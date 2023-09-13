64a2cc8ea0c6788b4fae5d0308ef2ae5e187d215
w-garden <w.garden316@gmail.com>
2023-09-13 23:44:21 +0900

    대역사용해서 약한 암호 확인하기

---
+++ tdd_start/src/test/java/chap07/UserRegisterTest.java
@@ -0,0 +1,28 @@
+package chap07;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserRegisterTest {
+    private UserRegister userRegister;
+    private StubWeakPasswordChecker stubWeakPasswordChecker = new StubWeakPasswordChecker();
+
+    @BeforeEach
+    void setUp() {
+        userRegister = new UserRegister(stubWeakPasswordChecker);
+    }
+
+    @DisplayName("약한 암호면 가입 실패")
+    @Test
+    void weakPassword() {
+        stubWeakPasswordChecker.setWeak(true); //약한 암호로 설정
+
+        assertThrows(WeakPasswordException.class, () -> {
+            userRegister.register("id", "pw", "email");
+        });
+    }
+}
