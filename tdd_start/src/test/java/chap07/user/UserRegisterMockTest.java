bc56218c3942a3f163dcd63d7cebf07f389b4031
w-garden <w.garden316@gmail.com>
2023-09-23 01:02:51 +0900

    Mockito를 이용해서 UserRegister 테스트

--- tdd_start/src/test/java/chap07/user/UserRegisterMockTest.java
+++ tdd_start/src/test/java/chap07/user/UserRegisterMockTest.java
@@ -1,13 +1,14 @@
 package chap07.user;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
-import org.mockito.Mockito;
+import org.mockito.ArgumentCaptor;
 
-import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.*;
 
 public class UserRegisterMockTest {
     private UserRegister userRegister;
@@ -23,10 +24,36 @@ public class UserRegisterMockTest {
     @DisplayName("약한 암호면 가입 실패")
     @Test
     void weakPassword(){
-        BDDMockito.given(mockPasswordChecker.checkPasswordWeak("pw")).willReturn(true);
+        given(mockPasswordChecker.checkPasswordWeak("pw")).willReturn(true);
 
-        Assertions.assertThrows(WeakPasswordException.class, () ->{
+        assertThrows(WeakPasswordException.class, () ->{
             userRegister.register("id","pw","email");
         });
+    }
+
+    @DisplayName("회원 가입시 암호 검사 수행함")
+    @Test
+    void checkPassword(){
+        userRegister.register("id", "pw", "email");
+
+        //then () : 인자로 전달한 mockPasswordChecker 모의 객체의 
+        //should() : 특정 메서드가 호출 되었는지 검증
+        //anyString() : 임의의 String 타입 인자를 이용해서 checkPasswordWeak() 메서드 호출 여부를 확인
+        then(mockPasswordChecker)
+                .should()
+                .checkPasswordWeak(anyString());
+    }
+
+    @DisplayName("가입하면 메일을 전송함")
+    @Test
+    void whenRegisterThenSendMail(){
+        userRegister.register("id","pw","email@email.com");
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class); //모의 객체를 메서드를 호출할 때 전달한 객체를 담는 기능을 제공
+        then(mockEmailNotifier)
+                .should().sendRegisterEmail(captor.capture());
+
+        String realEmail = captor.getValue();
+        assertEquals("email@email.com", realEmail);
     }
 }
