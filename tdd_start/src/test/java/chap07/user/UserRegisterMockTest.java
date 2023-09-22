6f913510582e75ff40ef73c531dea8c99414bf4f
w-garden <w.garden316@gmail.com>
2023-09-22 18:17:22 +0900

    스텁을 Mockito로 교체

--- tdd_start/src/test/java/chap07/user/UserRegisterMockTest.java
+++ tdd_start/src/test/java/chap07/user/UserRegisterMockTest.java
@@ -1,4 +1,32 @@
 package chap07.user;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
 public class UserRegisterMockTest {
+    private UserRegister userRegister;
+    private WeakPasswordChecker mockPasswordChecker = mock(WeakPasswordChecker.class);
+    private MemoryUserRepository fakeRepository = new MemoryUserRepository();
+    private EmailNotifier mockEmailNotifier = mock(EmailNotifier.class);
+
+    @BeforeEach
+    void setUp() {
+        userRegister = new UserRegister(mockPasswordChecker, fakeRepository, mockEmailNotifier);
+    }
+
+    @DisplayName("약한 암호면 가입 실패")
+    @Test
+    void weakPassword(){
+        BDDMockito.given(mockPasswordChecker.checkPasswordWeak("pw")).willReturn(true);
+
+        Assertions.assertThrows(WeakPasswordException.class, () ->{
+            userRegister.register("id","pw","email");
+        });
+    }
 }
