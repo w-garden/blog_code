f1ce4ba58c1e6143ef0d979c20dae976e825d387
w-garden <w.garden316@gmail.com>
2023-10-06 23:48:18 +0900

    Stream예제

---
+++ basic_java/src/ch14/StreamEx1.java
@@ -0,0 +1,55 @@
+package ch14;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class StreamEx1 {
+    public static void main(String[] args) {
+        Stream<Student> studentStream = Stream.of(
+                new Student("이자바", 2, 300),
+                new Student("김자바", 1, 200),
+                new Student("안자바", 4, 500),
+                new Student("박자바", 2, 100),
+                new Student("소자바", 5, 500),
+                new Student("최자바", 1, 400),
+                new Student("최자바", 2, 900)
+        );
+
+        studentStream.sorted(Comparator.comparing(Student::getBan)
+                        .thenComparing(Comparator.naturalOrder()))
+                .forEach(System.out::println);
+    }
+}
+
+
+class Student implements Comparable<Student> {
+    String name;
+    int ban;
+    int totalScore;
+
+    public Student(String name, int ban, int totalScore) {
+        this.name = name;
+        this.ban = ban;
+        this.totalScore = totalScore;
+    }
+
+    public String toString() {
+        return String.format("[%s, %d %d]", name, ban, totalScore);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBan() {
+        return ban;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public int compareTo(Student s) {
+        return s.totalScore - this.totalScore;
+    }
+}
