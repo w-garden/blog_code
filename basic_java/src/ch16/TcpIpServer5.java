b2edae0f69712053fb343e5fc6156c68e51befd1
w-garden <w.garden316@gmail.com>
2023-10-12 23:58:08 +0900

    TcpIP Server <-> Client 통신 예제

---
+++ basic_java/src/ch16/TcpIpServer5.java
@@ -0,0 +1,87 @@
+package ch16;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class TcpIpServer5 {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        try {
+            serverSocket = new ServerSocket(7777);
+            System.out.println("서버가 준비되었습니다.");
+
+            socket = serverSocket.accept();
+
+            Sender sender = new Sender(socket);
+            Receiver receiver = new Receiver(socket);
+
+            sender.start();
+            receiver.start();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Sender extends Thread {
+        Socket socket;
+        DataOutputStream out;
+        String name;
+
+
+        @Override
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+            while (out != null) {
+                try {
+                    out.writeUTF(name + scanner.nextLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            super.run();
+        }
+
+        public Sender(Socket socket) {
+            this.socket = socket;
+            try {
+                out = new DataOutputStream(socket.getOutputStream());
+                name = "[" + socket.getInetAddress() + " : " + socket.getPort() + "]";
+            } catch (IOException e) {
+            }
+
+        }
+    }
+
+    private static class Receiver extends Thread {
+        Socket socket;
+        DataInputStream in;
+
+        public Receiver(Socket socket) {
+            this.socket = socket;
+            try {
+                in = new DataInputStream(socket.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            while (in != null) {
+                try {
+                    System.out.println(in.readUTF());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
