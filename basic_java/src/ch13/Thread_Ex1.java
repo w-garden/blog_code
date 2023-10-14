347c4036e6f1ddc84c02a68bc8e69699d264d0a6
w-garden <w.garden316@gmail.com>
2023-10-15 00:54:18 +0900

    Thread예제 수정

--- basic_java/src/ch13/Thread_Ex1.java
+++ basic_java/src/ch13/Thread_Ex1.java
@@ -1,13 +1,17 @@
 package ch13;
 
 class Thread_Ex1 extends Thread {
+    public static void main(String[] args) {
+        Thread_Ex1 t = new Thread_Ex1();
+        t.start();
+    }
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println(getName());
+            System.out.println(i+1+" "+getName());
             try {
-                sleep(1000);
+                sleep(300);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
