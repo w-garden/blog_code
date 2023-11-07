5a625352e3227459c97988a7ecb858fbce078345
w-garden <w.garden316@gmail.com>
2023-11-07 23:40:46 +0900

    의존관계 설정하기

---
+++ toby_spring/springbook/user/dao/UserDaoConnectionMaker.java
@@ -0,0 +1,29 @@
+package springbook.user.dao;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoConnectionMaker {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("wgarden");
+        user.setName("호처리");
+        user.setPassword("7777");
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println("user2.getName() : " + user2.getName());
+        System.out.println("user2.getPassword) : " + user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회성공");
+        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println("Connection counter : " + ccm.getCounter());
+    }
+}
